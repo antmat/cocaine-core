4922000d9d5d88d83e0c07121327f9a241c64883
Anton Matveenko <antmat@me.com>
2017-11-13 14:51:41 +0300

    fix: extend test

--- tests/unit/context.cpp
+++ tests/unit/context.cpp
@@ -44,6 +44,31 @@ TEST(context, bucket_random_dispatcher) {
     ASSERT_NEAR(counter[values[1]], 1000, 50);
     ASSERT_NEAR(counter[values[2]], 1000, 50);
     ASSERT_EQ(counter[values[3]], 0);
+
+    // One value with lowest value
+    values[0] = 0.1;
+    values[1] = 0.21;
+    values[2] = 0.22;
+    values[3] = 0.23;
+    sample = std::vector<engine_mock_t>(values.begin(), values.end());
+    counter.clear();
+    for(size_t i = 0; i < 3000; i++) {
+        counter[d->next(sample)->utilization()]++;
+    }
+    ASSERT_EQ(counter[values[0]], 3000);
+
+    // Extend bucket size, use the same sample
+    counter.clear();
+    dynamic_t::object_t args;
+    args["bucket_size"] = 0.3;
+    d = new_distributor<std::vector<engine_mock_t>>("bucket_random", args);
+    for(size_t i = 0; i < 3000; i++) {
+        counter[d->next(sample)->utilization()]++;
+    }
+    EXPECT_NEAR(counter[values[0]], 750, 50);
+    EXPECT_NEAR(counter[values[1]], 750, 50);
+    EXPECT_NEAR(counter[values[2]], 750, 50);
+    EXPECT_NEAR(counter[values[3]], 750, 50);
 }
 
 } // namespace
